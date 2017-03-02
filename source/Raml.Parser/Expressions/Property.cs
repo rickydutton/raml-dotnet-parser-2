57b2e15eb02c6fbefcf50d1b690e5fac52cfe826
rickydutton <ricky.dutton@icheque.com>
2017-03-02 12:11:55 +0000

    Revert "A"

--- source/Raml.Parser/Expressions/Property.cs
+++ source/Raml.Parser/Expressions/Property.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace Raml.Parser.Expressions
 {
@@ -13,20 +12,5 @@ namespace Raml.Parser.Expressions
 
         // file
         public ICollection<string> FileTypes { get; set; }
-
-        private string _libraryName;
-        public new string LibraryName
-        {
-            get
-            {
-                if (_libraryName == null && !string.IsNullOrEmpty(Type) && Type.Contains("."))
-                    _libraryName = Type.Remove(Type.LastIndexOf(".", StringComparison.Ordinal));
-                return _libraryName;
-            }
-            set
-            {
-                _libraryName = value;
-            }
-        }
     }
 }
